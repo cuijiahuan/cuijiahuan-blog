5c92f9907f0bc80129162aef69ad559fbb971848
jiahuanc <jihuanc_3@163.com>
2020-12-21 17:21:00 +0800

    修改脚本

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,10 @@
-# 构建
-npm run build
-# 导航到构建输出目录
-cd docs/.vuepress/dist
+:: 导航到构建输出目录
+copy C:\Users\cuijiahuan\Documents\GitHub\cuijiahuan-blog\public C:\Users\cuijiahuan\Documents\GitHub\cuijiahuan.github.io
+cd C:\Users\cuijiahuan\Documents\GitHub\cuijiahuan.github.io
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 推到你仓库的 master 分支
-git push -f git@github.com:cuijiahuan/cuijiahuan.github.io.git master
+:: 推到仓库的 master 分支
+git push -f https://github.com/cuijiahuan/cuijiahuan.github.io master
