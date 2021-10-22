ec2ddda26bf0fabf649122b9c11fb424a57a2d36
clarecui <clarecui@tencent.com>
2021-10-22 15:01:28 +0800

    保存修改内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,5 +13,17 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
-  plugins: ['@vuepress/medium-zoom', 'flowchart'] 
+  plugins: [
+    // 趣味标题插件
+    [
+      'dynamic-title',
+      {
+         showIcon: '/favicon.ico',
+         showText: '(/≧▽≦/)咦！又好了！',
+         hideIcon: '/failure.ico',
+         hideText: '(●—●)喔哟，崩溃啦！',
+         recoverTime: 2000,
+      },
+    ],
+  ] 
 }  
