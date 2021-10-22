60045dc60f7a206d7d4acfda189ca7c3e6674d91
clarecui <clarecui@tencent.com>
2021-10-22 11:04:38 +0800

    保存修改内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,15 +9,7 @@ module.exports = {
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
   theme: 'reco',
-  themeConfig: {
-    mode: 'dark',
-    modePicker: true,
-    valineConfig: {
-      appId: 'zjR4dMTpvo0zEJOnkSqXinYX-gzGzoHsz',
-      appKey: 'cXH8sBPviOXPfM7VGiQYeMxY',
-    }
-
-  },
+  themeConfig,
   markdown: {
     lineNumbers: true
   },
--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -11,4 +11,12 @@ module.exports = Object.assign({}, themeReco, {
   searchMaxSuggestions: 10,
   // 自动形成侧边导航
   sidebar: 'auto',
+  // 暗色模式
+  mode: 'auto',
+  modePicker: true,
+  // 评论
+  valineConfig: {
+    appId: 'zjR4dMTpvo0zEJOnkSqXinYX-gzGzoHsz',// your appId
+    appKey: 'cXH8sBPviOXPfM7VGiQYeMxY', // your appKey
+  }
 })
