7e9d0d8a611b966d1d263fea7f19e30df3e6f4fe
cuijiahuan <cuijiahuan97@foxmail.com>
2023-06-02 16:14:42 +0800

    更新blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,16 +57,16 @@ module.exports = {
     //   }
     // ],
     // 趣味标题插件
-    [
-      'dynamic-title',
-      {
-         showIcon: '/favicon.ico',
-         showText: '(/≧▽≦/)咦！又好了！',
-         hideIcon: '/failure.ico',
-         hideText: '(●—●)喔哟，崩溃啦！',
-         recoverTime: 2000,
-      },
-    ],
+    // [
+    //   'dynamic-title',
+    //   {
+    //      showIcon: '/favicon.ico',
+    //      showText: '(/≧▽≦/)咦！又好了！',
+    //      hideIcon: '/failure.ico',
+    //      hideText: '(●—●)喔哟，崩溃啦！',
+    //      recoverTime: 2000,
+    //   },
+    // ],
     [
       '@vuepress/pwa', 
       {
--- docs/.vuepress/config/theme/themeReco.js
+++ docs/.vuepress/config/theme/themeReco.js
@@ -14,7 +14,7 @@ module.exports = {
   // 最后更新时间
   lastUpdated: 'Last Updated', // string | boolean
   // 作者
-  author: '柚宝',
+  author: '鱼鱼大仙的猫',
   authorAvatar: '/head.png',
   // 备案号
   record: '',
